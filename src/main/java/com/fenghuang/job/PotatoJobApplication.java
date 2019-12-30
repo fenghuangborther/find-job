f79e1a4709c3f759cb303cf36eec68e4b955f319
luoxian <15290810931@163.com>
2019-12-30 22:15:19 +0800

    #创建项目

---
+++ src/main/java/com/fenghuang/job/PotatoJobApplication.java
@@ -0,0 +1,13 @@
+package com.fenghuang.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( PotatoJobApplication.class, args );
+    }
+
+}
